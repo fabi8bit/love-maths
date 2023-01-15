3b631ed13e8b2ede362befb533284ffdf393d802
fabi8bit <fabi8bit@gmail.com>
2023-01-15 08:38:24 +0000

    Add event listeners for document loading and buttons clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and event listener to them
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = this.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You clicked submit!');
+            } else {
+                let GameType = this.getAttribute('data-type');
+                alert(`You clicked ${GameType}`);
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
@@ -31,5 +50,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivideQuestion() {
-    
+
 }
